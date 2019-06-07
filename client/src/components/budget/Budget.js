6eb26ab0be13664447fad9489cab6954b03c5272
Alex Beeson <beeson.alexander@gmail.com>
2019-06-07 16:04:33 -0400

    set up budget component

--- client/src/components/budget/Budget.js
+++ client/src/components/budget/Budget.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import NavBarContent from '../nav-bar/NavBarContent';
 
 export class Budget extends Component {
     render() {
         return (
+            <>
+            <NavBarContent />
             <div>
                 
             </div>
+            </>
         )
     }
 }
